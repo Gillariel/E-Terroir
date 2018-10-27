a98e6238e2279c946f7c1fbe11a88f78d7a98080
Kosta <k.sovaridis@gmail.com>
2018-10-27 20:15:32 +0200

    Added: Navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faMale } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookF, faTwitter} from '@fortawesome/free-brands-svg-icons';
 import { faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+import Header from './components/Header';
 import Footer from './components/Footer';
 
 import {PageHeader} from 'react-bootstrap';
@@ -18,31 +19,18 @@ library.add(faTwitter);
 
 class App extends Component {
 
-  
+
   render() {
-   
+
     return (
-      <div className="App">
-       {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-    </header>*/}
-    
-    <AppRouter></AppRouter>
-    <Footer></Footer>
-      
-      
-      </div>
+        <div className="App">
+        <Header/>
+
+        <AppRouter></AppRouter>
+        <Footer></Footer>
+
+
+        </div>
     );
   }
 }
--- src/screens/AppRouter.js
+++ src/screens/AppRouter.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import Login from './Login';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {Grid,Row,Col, Nav} from 'react-bootstrap';
+import Header from '../components/Header';
 
 
 
@@ -11,42 +12,21 @@ import {Grid,Row,Col, Nav} from 'react-bootstrap';
 const AppRouter = () => {
   return(
     <BrowserRouter>
-<div>
-  
-  <Grid>
-          
-            <Row>
-            <Col md={8}><Link to='/'><img src={require('../Logo_terroir.png')} style={{height:"80%", width:"60%"}} alt="Favicon"></img></Link></Col>
-            <Col md={2}><Link to='/login'><FontAwesomeIcon icon = "male" /></Link></Col>
-            <Col md={2}><Link to='/Panier'><FontAwesomeIcon icon = "shopping-cart"/></Link></Col>
-            </Row>
-            <Nav /*onSelect={this.handleSelect}*/ >
-                <NavLink exact to='/' class="navitem" eventKey={1}>Acceuil</NavLink>
-                <a class="navitem" eventKey={2} href="#">Catalogue</a>
-            </Nav>
 
-           
-            </Grid>
-  <Switch>
-      <Route exact path='/' component={Home} />
-      <Route path='/login' component={Login} />
-      
-    </Switch>
-</div>
- 
-
-</BrowserRouter>
-
-
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/login' component={Login} />
+        </Switch>
+        
+    </BrowserRouter>
   );
 
 
 }
-   
-     
-    
-  
+
+
+
+
 
 
 export default AppRouter;
-
