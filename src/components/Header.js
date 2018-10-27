4860f1f55b276930d63a7ab2540b62c690cc9288
Thi Phuong Lien LE <lienphuong0504@gmail.com>
2018-10-27 07:47:22 +0200

    favicon fait

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,7 @@ export default class Header extends Component{
     render(){
         return(
             <div style={{display: 'inline-flex'}}>
-            <img src="Logo_terroir.png" alt="Favicon"></img>
-            <h1>E-Terroir</h1>
+            <img src={require('../Logo_terroir.png')} alt="Favicon"></img>
             <a><FontAwesomeIcon icon = "male"/></a>
             <a><FontAwesomeIcon icon = "shopping-cart"/></a>
             </div>
